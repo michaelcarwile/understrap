126fc9fcdfcb63ce2f8beb11e3236ccea7def30d
Michael Carwile <michael@carwile.org>
2018-10-24 09:41:41 -0500

    Add the hero widget to front_page

--- page.php
+++ page.php
@@ -20,6 +20,10 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
 ?>
 
+<?php if ( is_front_page() ) : ?>
+  <?php get_template_part( 'global-templates/hero' ); ?>
+<?php endif; ?>
+
 <div class="wrapper" id="page-wrapper">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
